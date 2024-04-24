dc7d270b41481a6dff66962b9152555a3d6d5978
BreezyCave <s.toku@nemro.jp>
2024-04-24 16:30:29 +0900

    240424 test10

--- R/Axdistmap001.R
+++ R/Axdistmap001.R
@@ -1,10 +1,10 @@
 #' @title creating distance map and binary image from tiff image file
 #' @description \code{Axdistmap} create images of distance map and binary image (option) from tiff image files
-#' @import EBImage
 #' @import stringr
-#' @import dplyr
 #' @import png
 #' @import colorspace
+#' @import EBImage
+#' @importFrom dplyr mutate
 #' @importFrom utils write.table
 #' @importFrom utils choose.files
 #' @importFrom stats na.omit
@@ -26,7 +26,7 @@ Axdistmap <- function( Binary = FALSE, All_Features = FALSE){
     str_split( "/") %>%
     unlist() %>%
     as.data.frame()  %>%
-    mutate(level = row_number())
+    dplyr::mutate(level = row_number())
   colnames(dir_info) <- c("Dir_Name", "Level")
   # 最後のファイル名を除いた部分を抽出
   Mainf <- str_c(dir_info$Dir_Name[1:(nrow(dir_info)-1)], collapse = "/")
@@ -45,29 +45,29 @@ Axdistmap <- function( Binary = FALSE, All_Features = FALSE){
   is.tif <- function(x) regexpr('\\.tif$', x) + regexpr('\\.tiff$', x)> 0
 
   for (file_list.name in list.files()[is.tif(list.files())]){
-      test <- readImage(file_list.name)
-      test <- resize(test, w = 900)#15Jun11:696 22Apr15:900
+      test <- EBImage::readImage(file_list.name)
+      test <- EBImage::resize(test, w = 900)#15Jun11:696 22Apr15:900
       test_s <- (1/(1+(0.5/test[,,1])^5))
-      mf1 <- medianFilter(test_s,1)
+      mf1 <- EBImage::medianFilter(test_s,1)
       x_mf1 <- (log1p(mf1)/log1p(max(mf1)))
       x_mf1_log <- (log1p(x_mf1)/log1p(max(x_mf1)))
-      x_thr <- thresh(x_mf1_log, 8, 8, -0.04)
-      kern <- makeBrush(1, shape="box")
-      x_thr_bw <- 1 - opening(closing(x_thr, kern), kern)
-      dm <- distmap(x_thr_bw)
-      ddm <- normalize(dm)
-      dml <- bwlabel(ddm)
-      sdat <- computeFeatures.shape(dml)
+      x_thr <- EBImage::thresh(x_mf1_log, 8, 8, -0.04)
+      kern <- EBImage::makeBrush(1, shape="box")
+      x_thr_bw <- 1 - EBImage::opening(closing(x_thr, kern), kern)
+      dm <- EBImage::distmap(x_thr_bw)
+      ddm <- EBImage::normalize(dm)
+      dml <- EBImage::bwlabel(ddm)
+      sdat <- EBImage::computeFeatures.shape(dml)
       #######Threshold for eliminating small objects that cannot be classified by image processing
       rma <- 30
-      rmNum <- which(data_sht$s.area <= rma)
-      dml <- rmObjects(dml, rmNum)
-      sdat <- computeFeatures.shape(dml) #option with
+      rmNum <- which(sdat$s.area <= rma)
+      dml <- EBImage::rmObjects(dml, rmNum)
+      sdat <- EBImage::computeFeatures.shape(dml) #option with
       ####this elimination option is not included in the original program
       #colorMode(dml) <- Grayscale
 
-      hdat <- computeFeatures.haralick(dml,ddm)
-      mdat <- computeFeatures.moment(dml)
+      hdat <- EBImage::computeFeatures.haralick(dml,ddm)
+      mdat <- EBImage::computeFeatures.moment(dml)
 
       data_sht <<- data.frame(cbind( sdat, hdat, mdat))
 
